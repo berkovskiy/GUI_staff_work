20f1d8d9dedc9f1d81b70dfe8649e281522ea20d
Artem <berkovskiymzk@gmail.com>
2020-06-08 20:14:08 +0700

    Доработан вопрос о проверке введенного логина и пароля пользователя. Реализована возможность открытия дочернего окна и закрытия окна прохождения авторизации

--- create_Outfit.py
+++ create_Outfit.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QMessageBox
 from designer.login import Ui_MainWindow  # импорт нашего сгенерированного файла
+from designer.lk_admin import Ui_LK_admin
 import sys
 
 from bd.queries_to_the_DB import serch_user_profile
@@ -24,20 +25,25 @@ class Mywindow(QtWidgets.QMainWindow):
             for i in serch_user_profile(user_line):
                 self.user = i[0]
                 self.password = i[1]
-            if user_line != self.user or password_line != self.password:
-                QMessageBox.critical(self, 'Ошибка авторизации', "Введите корректные логин и пароль")
-            else:
-                Log_oute()
+            if user_line == self.user or password_line == self.password:
+                self.close()
+                self.two_windo = Lk_admin()
+                self.two_windo.show()
         except Exception:
-            QMessageBox.critical(self, 'Ошибка авторизации', "Введите хоть что нибудь")
+            QMessageBox.critical(self, 'Ошибка авторизации', "Ввведены не корректный логин и пароль")
 
 
-class Log_oute(QtWidgets.QMainWindow):
+class Lk_admin(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Log_oute, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        res = QMessageBox.question(self, "Авторизация пройдена", "Вы успешно прошли авторизацию", QMessageBox.Ok)
+        super(Lk_admin, self).__init__()
+        self.lk = Ui_LK_admin()
+        self.lk.setupUi(self)
+        QMessageBox.question(self, "Авторизация пройдена", "Вы успешно прошли авторизацию", QMessageBox.Ok)
+        self.lk.pushButton.clicked.connect(self.but_one)
+
+    def but_one(self):
+        print('Второе окно открылось!')
+
 
 
 
