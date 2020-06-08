78346a7b348ac3acb2e4d17d209328bba92c0d4f
Artem <berkovskiymzk@gmail.com>
2020-06-08 20:04:09 +0700

    Решена проблема с невведением логина и пароля пользователей. Будет всплывать предупреждение, что требуется ввести какие либо значения в поле "Логин/пароль"

--- create_Outfit.py
+++ create_Outfit.py
@@ -1,9 +1,7 @@
-from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtWidgets import QMessageBox, qApp
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMessageBox
 from designer.login import Ui_MainWindow  # импорт нашего сгенерированного файла
 import sys
-
-
 
 from bd.queries_to_the_DB import serch_user_profile
 
@@ -17,21 +15,21 @@ class Mywindow(QtWidgets.QMainWindow):
         self.ui.lineEdit_2.setMaxLength(15)
         self.ui.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
         self.ui.pushButton.clicked.connect(self.data_checking)
-        self.ui.pushButton_2.clicked.connect(qApp.quit)
 
     # Метод проверки, есть такой логин и пароль или нет
     def data_checking(self):
-        user_line = self.ui.lineEdit.text()
-        password_line = self.ui.lineEdit_2.text()
-        for i in serch_user_profile(user_line):
-            self.user = i[0]
-            self.password = i[1]
-        if user_line != self.user or password_line != self.password:
-            QMessageBox.critical(self, 'Ошибка авторизации', "Введите корректные логин и пароль")
-        else:
-
-            Log_oute()
-
+        try:
+            user_line = self.ui.lineEdit.text()
+            password_line = self.ui.lineEdit_2.text()
+            for i in serch_user_profile(user_line):
+                self.user = i[0]
+                self.password = i[1]
+            if user_line != self.user or password_line != self.password:
+                QMessageBox.critical(self, 'Ошибка авторизации', "Введите корректные логин и пароль")
+            else:
+                Log_oute()
+        except Exception:
+            QMessageBox.critical(self, 'Ошибка авторизации', "Введите хоть что нибудь")
 
 
 class Log_oute(QtWidgets.QMainWindow):
@@ -40,9 +38,6 @@ class Log_oute(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         res = QMessageBox.question(self, "Авторизация пройдена", "Вы успешно прошли авторизацию", QMessageBox.Ok)
-        qApp.quit()
-
-
 
 
 
